983fc8f42ed1a094301710c4feff1464898c1402
chrismetz09 <christophermetz@Christophers-MacBook-Air.local>
2019-01-08 20:37:54 -0800

    2nd commit
    Signed-off-by: chrismetz09 <christophermetz@Christophers-MacBook-Air.local>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = function () {
-   return <div>Hi there!</div>;
-};
+   return (
+   <div>
+      <label class="label" for="name">
+      Enter name:
+      </label>
+      <input id="name" type="text"/>
+      <button style = {{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+   </div>
+   );
+   };
 
 ReactDOM.render(
    <App />,
